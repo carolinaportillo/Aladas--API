85e1eb8bcd55af00b8e765cc75d786ac2f10cab2
carolinaportillo <carolinaa.portillo@hotmail.com>
2021-07-24 20:47:01 -0300

    Se crean enumerados para los estados de vuelo y reservas, se modifican los getters y setters de los atributos que poseen enumerados en ambas entities. Se crea la clase Pasaje.java y se establece la relacion oneToOne entre esta y la clase Reserva.java , tambien se agrega la relacion bidireccional entre ambas en Reserva.java creando un metodo alternativo para asociar el pasaje con la reserva; otra manera valida y planteada es por medio de la modificacion del setter del atributo pasaje.  Se realiza una correcion en el JoinColumn del atributo reserva en la clase Pasaje.java.

--- src/main/java/ar/com/ada/api/aladas/entities/Reserva.java
+++ src/main/java/ar/com/ada/api/aladas/entities/Reserva.java
@@ -20,6 +20,12 @@ public class Reserva {
     @JoinColumn(name = "pasajero_id", referencedColumnName = "pasajero_id")
     private Pasajero pasajero;
 
+    //se establece la relacion entre ambos objetos (reserva y pasaje)
+    //nombre del atributo que hace ref a la tabla
+    @OneToOne(mappedBy = "reserva", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private Pasaje pasaje;
+    
+
     @Column(name = "estado_reserva_id")
     private Integer estadoReservaId;
 
@@ -53,12 +59,21 @@ public class Reserva {
         this.pasajero = pasajero;
     }
 
-    public Integer getEstadoReservaId() {
-        return estadoReservaId;
+    public Pasaje getPasaje() {
+        return pasaje;
     }
 
-    public void setEstadoReservaId(Integer estadoReservaId) {
-        this.estadoReservaId = estadoReservaId;
+    public void setPasaje(Pasaje pasaje) {
+        this.pasaje = pasaje;
+        //pasaje.setReserva(this); con esta linea se puede establecer la RB del oneToOne
+    }                              //entre reserva y pasaje a traves del setter.
+
+    public EstadoReservaEnum getEstadoReservaId() {
+        return EstadoReservaEnum.parse(estadoReservaId);
+    }
+
+    public void setEstadoReservaId(EstadoReservaEnum estadoReservaId) {
+        this.estadoReservaId = estadoReservaId.getValue();
     }
 
     public Date getFechaEmision() {
@@ -75,6 +90,44 @@ public class Reserva {
 
     public void setFechaVencimiento(Date fechaVencimiento) {
         this.fechaVencimiento = fechaVencimiento;
+    }
+
+    //metodo de asociacion alternativo para establecer la RB entre reserva y pasaje
+
+    public void asociarPasaje(Pasaje pasaje){
+        this.pasaje = pasaje;
+        pasaje.setReserva(this);
+    }
+
+
+
+
+    public enum EstadoReservaEnum {
+
+        CREADA(1), TRANSMITIENDO_AL_PG(2), ERROR_AL_CONECTAR_PG(3), PENDIENTE_DE_PAGO(4), PAGADA(5),
+        CANCELADO_POR_USUARIO(6), CANCELADO_POR_EMPRESA(7), PAGO_RECHAZADO(8), EXPIRADO(9), EMITIDA(10);
+
+        private final Integer value;
+
+        
+        private EstadoReservaEnum(Integer value) {
+            this.value = value;
+        }
+
+        public Integer getValue() {
+            return value;
+        }
+
+        public static EstadoReservaEnum parse(Integer id) {
+            EstadoReservaEnum status = null; 
+            for (EstadoReservaEnum item : EstadoReservaEnum.values()) {
+                if (item.getValue().equals(id)) {
+                    status = item;
+                    break;
+                }
+            }
+            return status;
+        }
     }
 
     
